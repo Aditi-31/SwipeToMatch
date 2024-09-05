2bb98233bed0cb530160028e2508a77081343b77
Aditi-31 <jain31071999aditi@gmail.com>
2024-09-05 21:29:44 +0530

    button display and action

--- SwipeToMatch/ContentView.swift
+++ SwipeToMatch/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  SwipeToMatch
-//
-//  Created by Aditi Jain on 03/09/24.
-//
-
 
 import SwiftUI
 import CoreData
@@ -43,49 +36,9 @@ struct ContentView: View {
                     if index < cardStates.count {
                         CardView(
                             user: user,
-                            cardState: $cardStates[index],
-                            onRemove: { removeUser(at: index) }
+                            cardState: $cardStates[index]
                         )
                         .padding(8)
-                    }
-                }
-                
-                // Bottom Stack
-                HStack(spacing: 0) {
-                    Button(action: {}) {
-                        Image("refresh")
-                    }
-                    Button(action: {
-                        if !cardStates.isEmpty {
-                            withAnimation(Animation.easeIn(duration: 0.8)) {
-                                let index = 0 // Assuming you want to affect the first card
-                                cardStates[index].xCoordinate = -500
-                                cardStates[index].degree = -12
-                                removeUser(at: index)
-                            }
-                        }
-                    }) {
-                        Image("dismiss")
-                    }
-                    Button(action: {}) {
-                        Image("super_like")
-                    }
-                    Button(action: {
-                        if !cardStates.isEmpty {
-                            let index = 0 // Assuming you want to affect the first card
-                            let likedUser = viewModel.userData[index]
-                            saveLikedUser(likedUser)
-                            withAnimation(Animation.easeIn(duration: 0.8)) {
-                                cardStates[index].xCoordinate = 500
-                                cardStates[index].degree = 12
-                                removeUser(at: index)
-                            }
-                        }
-                    }) {
-                        Image("like")
-                    }
-                    Button(action: {}) {
-                        Image("boost")
                     }
                 }
             }
@@ -105,43 +58,28 @@ struct ContentView: View {
     func initializeCardState() {
         cardStates = Array(repeating: CardState(), count: viewModel.userData.count)
     }
-    
-    func removeUser(at index: Int) {
-        if index < viewModel.userData.count {
-            viewModel.userData.remove(at: index)
-            cardStates.remove(at: index)
-        }
-    }
-    
-    func saveLikedUser(_ user: User) {
-        var likedUsers = getLikedUsers()
-        likedUsers.append(user)
-        
-        if let encoded = try? JSONEncoder().encode(likedUsers) {
-            UserDefaults.standard.set(encoded, forKey: "LikedUsers")
-        }
-    }
-    
-    func getLikedUsers() -> [User] {
-        if let data = UserDefaults.standard.data(forKey: "LikedUsers"),
-           let users = try? JSONDecoder().decode([User].self, from: data) {
-            return users
-        }
-        return []
-    }
-}
 
-struct CardState {
-    var xCoordinate: CGFloat = 0
-    var yCoordinate: CGFloat = 0
-    var degree: CGFloat = 0
+//    func saveLikedUser(_ user: User) {
+//        let context = PersistenceController.shared.container.viewContext
+//        let profile = SaveUserData(context: context)
+//        profile.name = user.name.first + " " + user.name.last
+//        profile.email = user.email
+//        profile.city = user.location.city
+//        profile.isAccepted = true
+//
+//        do {
+//            try context.save()
+//        } catch {
+//            print("Failed to save user: \(error.localizedDescription)")
+//        }
+//    }
+
 }
 
 struct CardView: View {
     let cardGradient = Gradient(colors: [Color.black.opacity(0), Color.black.opacity(0.5)])
     let user: User
     @Binding var cardState: CardState
-    let onRemove: () -> Void
     
     var body: some View {
         ZStack(alignment: .topLeading) {
@@ -176,14 +114,72 @@ struct CardView: View {
                 }
                 .padding()
                 .foregroundColor(.white)
+                
+                HStack(alignment: .center) {
+                    Spacer()
+                    Button(action: {
+                       print("dismiss selected")
+                        handleButtonPress(.rejected)
+                    }) {
+                        Image("dismiss")
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    Button(action: {
+                        print("accepted selected")
+
+                        handleButtonPress(.accepted)
+                    }) {
+                        Image("like")
+                    }
+                    .buttonStyle(PlainButtonStyle())
+
+                    Spacer()
+                }
+                .padding(.bottom, 20)
+                .opacity(cardState.action == .none ? 1 : 0) // Hide buttons after selection
+                
+                if cardState.action == .accepted {
+                    Text("Accepted")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(width: 350, height: 60, alignment: .center)
+                        .background(.green)
+                        .padding(.bottom, 15)
+                } else if cardState.action == .rejected {
+                    Text("Rejected")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(width: 350, height: 60, alignment: .center)
+                        .background(.red) // Optional background for visibility
+                        .padding(.bottom, 15)
+                }
             }
         }
+        .frame(width: 380, height: 450)
         .cornerRadius(8)
         .offset(x: cardState.xCoordinate, y: cardState.yCoordinate)
         .rotationEffect(.init(degrees: cardState.degree))
-
+    }
+    
+    func handleButtonPress(_ action: CardAction) {
+        guard cardState.action == .none else { return } // Ensure only one action happens
+        cardState.action = action
     }
 }
+
+struct CardState {
+    var xCoordinate: CGFloat = 0
+    var yCoordinate: CGFloat = 0
+    var degree: CGFloat = 0
+    var action: CardAction = .none // Add a default action
+}
+
+enum CardAction {
+    case none, accepted, rejected
+}
+
 
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
