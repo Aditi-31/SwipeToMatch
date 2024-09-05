8d57d9d891e8bbcbc7271e702980c2ac8c67d5be
Aditi-31 <jain31071999aditi@gmail.com>
2024-09-05 15:58:05 +0530

    Liner list applied

--- SwipeToMatch/ContentView.swift
+++ SwipeToMatch/ContentView.swift
@@ -5,19 +5,18 @@
 //  Created by Aditi Jain on 03/09/24.
 //
 
+
 import SwiftUI
 import CoreData
 import SDWebImageSwiftUI
+
 struct ContentView: View {
     @ObservedObject var viewModel = UserService()
-    @State private var activeCardIndex: Int = 0
     @State private var cardStates: [CardState] = []
-    @State private var userData: [User] = []
-    @State private var i: Int = 0
-    @State private var flag: Bool = true
+    
     var body: some View {
         VStack {
-            if viewModel.dataFlag == false {
+            if !viewModel.dataFlag {
                 ProgressView("Loading users...") // Loading indicator while data is fetched
             } else {
                 // Top Stack
@@ -36,24 +35,20 @@ struct ContentView: View {
                     Button(action: {}) {
                         Image("chats")
                     }
-                }.padding(.horizontal)
+                }
+                .padding(.horizontal)
                 
-                // Image Card View
-                ZStack {
-                    ForEach(viewModel.userData.indices, id: \.self) { index in
-                        if index < cardStates.count {
-                            
-                            CardView(
-                                user: viewModel.userData[index],
-                                i: $i,
-                                cardIndex: i,
-                                cardState: $cardStates[index]
-                            )
-                            .padding(8)
-                        }
+                // List View of Cards
+                List(Array(viewModel.userData.enumerated()), id: \.offset) { index, user in
+                    if index < cardStates.count {
+                        CardView(
+                            user: user,
+                            cardState: $cardStates[index],
+                            onRemove: { removeUser(at: index) }
+                        )
+                        .padding(8)
                     }
                 }
-                .zIndex(1.0)
                 
                 // Bottom Stack
                 HStack(spacing: 0) {
@@ -61,11 +56,12 @@ struct ContentView: View {
                         Image("refresh")
                     }
                     Button(action: {
-                        if i >= 0 {
+                        if !cardStates.isEmpty {
                             withAnimation(Animation.easeIn(duration: 0.8)) {
-                                cardStates[i].xCoordinate = -500
-                                cardStates[i].degree = -12
-                                i -= 1
+                                let index = 0 // Assuming you want to affect the first card
+                                cardStates[index].xCoordinate = -500
+                                cardStates[index].degree = -12
+                                removeUser(at: index)
                             }
                         }
                     }) {
@@ -75,13 +71,14 @@ struct ContentView: View {
                         Image("super_like")
                     }
                     Button(action: {
-                        if i >= 0 {
-                            let likedUser = viewModel.userData[i]
+                        if !cardStates.isEmpty {
+                            let index = 0 // Assuming you want to affect the first card
+                            let likedUser = viewModel.userData[index]
                             saveLikedUser(likedUser)
                             withAnimation(Animation.easeIn(duration: 0.8)) {
-                                cardStates[i].xCoordinate = 500
-                                cardStates[i].degree = 12
-                                i -= 1
+                                cardStates[index].xCoordinate = 500
+                                cardStates[index].degree = 12
+                                removeUser(at: index)
                             }
                         }
                     }) {
@@ -94,7 +91,6 @@ struct ContentView: View {
             }
         }
         .onAppear {
-            // Get user data count and populate cardStates
             if viewModel.dataFlag {
                 initializeCardState()
             }
@@ -107,12 +103,16 @@ struct ContentView: View {
     }
     
     func initializeCardState() {
-        
         cardStates = Array(repeating: CardState(), count: viewModel.userData.count)
-        i = viewModel.userData.count - 1
     }
     
-    // Storing the liked data in local
+    func removeUser(at index: Int) {
+        if index < viewModel.userData.count {
+            viewModel.userData.remove(at: index)
+            cardStates.remove(at: index)
+        }
+    }
+    
     func saveLikedUser(_ user: User) {
         var likedUsers = getLikedUsers()
         likedUsers.append(user)
@@ -140,9 +140,8 @@ struct CardState {
 struct CardView: View {
     let cardGradient = Gradient(colors: [Color.black.opacity(0), Color.black.opacity(0.5)])
     let user: User
-    @Binding var i: Int
-    let cardIndex: Int
     @Binding var cardState: CardState
+    let onRemove: () -> Void
     
     var body: some View {
         ZStack(alignment: .topLeading) {
@@ -153,84 +152,39 @@ struct CardView: View {
                 .frame(width: 150, height: 150)
                 .foregroundColor(.gray)
             
-            // Actual image
             WebImage(url: URL(string: imageUrl))
                 .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 380, height: 450)
+                .clipped()
             
             LinearGradient(gradient: cardGradient, startPoint: .top, endPoint: .bottom)
+            
             VStack {
                 Spacer()
                 VStack(alignment: .leading) {
                     HStack {
-                        Text("\(user.name.first) \(user.name.last)").font(.largeTitle).fontWeight(.bold)
-                        Text("\(user.dob.age)").font(.title)
+                        Text("\(user.name.first) \(user.name.last)")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        Text("\(user.dob.age)")
+                            .font(.title)
                     }
                     Text(user.email)
                     Text(user.location.city)
                     Text(user.cell)
                 }
+                .padding()
+                .foregroundColor(.white)
             }
-            .padding()
-            .foregroundColor(.white)
-            
-            HStack{
-                Image("yes")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width:150)
-                    .opacity(Double(cardState.xCoordinate/10 - 1))
-                Spacer()
-                Image("nope")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width:150)
-                    .opacity(Double((cardState.xCoordinate/10 * -1) - 1))
-            }
-        }
-        .onAppear {
-            i = cardIndex
         }
         .cornerRadius(8)
         .offset(x: cardState.xCoordinate, y: cardState.yCoordinate)
         .rotationEffect(.init(degrees: cardState.degree))
-        .gesture(
-            DragGesture()
-                .onChanged { value in
-                    withAnimation(.default) {
-                        cardState.xCoordinate = value.translation.width
-                        cardState.yCoordinate = value.translation.height
-                        cardState.degree = 7 * (value.translation.width > 0 ? 1 : -1)
-                        i = cardIndex
-                    }
-                }
-                .onEnded { value in
-                    withAnimation(.interpolatingSpring(mass: 1.0, stiffness: 50, damping: 8, initialVelocity: 0)) {
-                        switch value.translation.width {
-                        case 0...100:
-                            cardState.xCoordinate = 0
-                            cardState.yCoordinate = 0
-                            cardState.degree = 0
-                        case let x where x > 100:
-                            cardState.xCoordinate = 500
-                            cardState.degree = 12
-                        case (-100)...(-1):
-                            cardState.xCoordinate = -500
-                            cardState.degree = -12
-                        case let x where x < -100:
-                            cardState.xCoordinate = -500
-                            cardState.degree = -12
-                        default:
-                            cardState.xCoordinate = 0
-                            cardState.yCoordinate = 0
-                        }
-                        if i >= 0 {
-                            i = i - 1
-                        }
-                    }
-                }
-        )
+
     }
 }
+
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
 }
